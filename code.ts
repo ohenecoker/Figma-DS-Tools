d244fdcfebb5423d74d3c62b1fd4b49e8a0dffd2
ohenecoker <coker.christian@proton.me>
2023-05-01 12:05:17 -0400

    fix: remove white background

--- code.ts
+++ code.ts
@@ -3,21 +3,53 @@ figma.ui.resize(400, 300)
 
 
 figma.ui.onmessage = msg => {
-    if (msg.type === 'label') {
+    if (msg.type === 'label' || msg.type === 'exp') {
         for (const node of figma.currentPage.selection) {
-            const parentX = node.x
-            const parentY = msg.orientation == 'horizontal' && node.type === 'COMPONENT_SET' ? node.absoluteBoundingBox!.y - (48 * 2) : node.y - 48
+            let gap = 48
+            const originalPosition: Position = {
+                x: node.x,
+                y: node.y,
+                direction: msg.orientation === 'horizontal' ? 'Left' : 'Up'
+            }
+            if (msg.type === 'exp' && msg.orientation === 'horizontal') {
+                gap = 120
+            }
+            const parent = node.parent
+            let parentX = node.x
+            let parentY = msg.orientation == 'horizontal' && node.type === 'COMPONENT_SET' ? node.y - (gap * 2) : node.y - gap
             const fontName: FontName = {family: "Rubik", style: "Bold"}
-            addLabel(node.name, parentX, parentY, fontName, node.parent as FrameNode)
+            let frame = figma.createFrame()
+            let fills: Paint[] = []
+            frame.fills = fills
+            if (msg.type === 'exp') {
+                parent!.appendChild(frame)
+                frame.name = `Annotation Frame: ${node.name}`
+                frame.appendChild(node)
+                if (msg.orientation === 'horizontal') {
+                    frame.resize(node.width, node.height + gap)
+                } else {
+                    frame.resize(node.width, node.height + gap)
+                }
+                frame.clipsContent = false
+                node.y = frame.y + (frame.height - node.height)
+                node.x = frame.x
+                parentY = frame.y
+                parentX = frame.x
+            }
+            let topLabel = addLabel(node.name, parentX, parentY, fontName, parent as FrameNode, true, frame)
+            let longest = 0
             if (node.type === 'COMPONENT_SET') {
-                let longest = 0
-                const gap = 48
 
                 figma.loadFontAsync({family: "Inter", style: "Regular"}).then((_) => {
                     figma.loadFontAsync({family: "Rubik", style: "Regular"}).then(async (_) => {
                         longest = await getLongestTextLength(node, fontName).then(async value => {
                             return value
                         })
+                        if (msg.orientation === 'vertical' && msg.type === 'exp') {
+                            frame.resize(node.width + longest + gap, node.height + gap)
+                            node.x = frame.width - node.width
+                            topLabel.x = node.x
+                        }
                         console.log("longest: ", longest)
                         for (const item of node.children) {
                             let name = ""
@@ -38,12 +70,19 @@ figma.ui.onmessage = msg => {
                             console.log("length: ", length)
 
                             console.log(node.parent)
-                            let x = msg.orientation === 'horizontal' ? item.absoluteBoundingBox!.x + node.width : /*(node.width - length)*/node.x + (node.width - length) - (node.width) - gap
-                            const y = msg.orientation === 'horizontal' ? node.absoluteBoundingBox!.y - 48 : node.y + item.y
+                            let x = msg.orientation === 'horizontal' ? node.x + (item.x) : node.x + (node.width - length) - (node.width) - gap
+                            const y = msg.orientation === 'horizontal' ? node.y - (gap / 2) : node.y + item.y
                             addLabel(name, x, y, fontName, node.parent as FrameNode)
                         }
                     })
                 })
+            }
+            if (msg.orientation === 'horizontal') {
+                frame.x = originalPosition.x
+                frame.y = originalPosition.y - (frame.height / 2)
+            } else {
+                frame.x = originalPosition.x - ((frame.width + gap + longest) / 2)
+                frame.y = originalPosition.y
             }
         }
 
@@ -147,18 +186,28 @@ function addPointers(position: Position, size: Size, number: number = 0) {
 }
 
 
-function addLabel(name: string, x: number, y: number, fontName: FontName, parent?: SceneNode) {
+function addLabel(name: string,
+                  x: number,
+                  y: number,
+                  fontName: FontName,
+                  parentFrame?: SceneNode,
+                  wrapInFrame: boolean = false,
+                  frameNode?: FrameNode
+) {
+    let text = figma.createText()
     figma.loadFontAsync({family: "Inter", style: "Regular"}).then((_) => {
         figma.loadFontAsync(fontName).then((_) => {
             let cv = (x: any) => x / 255
             const color: RGB = {r: cv(151), g: cv(71), b: cv(255)}
-            let text = figma.createText()
             text.name = "Annotation Label"
             text.y = y
             text.x = x ??= 0
-            if (parent != undefined) {
-                if (parent.type == 'FRAME') {
-                    parent.appendChild(text)
+            // let frame =
+            if (wrapInFrame == true) {
+                frameNode!.appendChild(text)
+            } else if (parentFrame != undefined) {
+                if (parentFrame.type == 'FRAME') {
+                    parentFrame.appendChild(text)
                 }
             } else {
                 figma.currentPage.appendChild(text)
@@ -169,6 +218,7 @@ function addLabel(name: string, x: number, y: number, fontName: FontName, parent
             text.textAlignHorizontal = "RIGHT"
         })
     })
+    return text
 }
 
 async function getLongestTextLength(node: ComponentSetNode, fontName: FontName) {
